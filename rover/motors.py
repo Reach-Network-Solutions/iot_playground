88550e2b46fc9aa714645010961da84105e619ef
sondeche-rns <stephen.ondeche@reachnetworks.net>
2021-04-27 20:26:51 +0300

    add useful comments

--- rover/motors.py
+++ rover/motors.py
@@ -3,7 +3,9 @@ from time import sleep
 
 class Motor():
     def __init__(self, leftPin, rightPin, orientation="right"):
-        #the pins are always regarded as right and left for the motor
+        # The pins are always regarded as left and right respectively  for the motor
+        # The right pin should always be connected to the ground NC connection
+        # The left pin should always be connected to the live NO connection
         self.orientation = orientation
         if orientation == "right":
             self.activePin = rightPin
@@ -14,36 +16,43 @@ class Motor():
         
         gpio.setmode(gpio.BCM)
 
+        # Set the PINs as output pins
         gpio.setup(self.activePin, gpio.OUT)
         gpio.setup(self.groundPin, gpio.OUT)
+
+        # The pins always start in a high state
+        # Set each pin to the low mode to reset them
         self.off(self.groundPin)
         self.off(self.activePin)
         
     def on(self, pin):
+        # Simple on command to create abstraction for the confusing HIGH and LOW GPIO status
         gpio.output(pin, gpio.LOW)
 
     def off(self, pin):
+        # Simple on command to create abstraction for the confusing HIGH and LOW GPIO status
         gpio.output(pin, gpio.HIGH)
 
     def move_forward(self):
         print("Moving motor forward...")
-        #turn off ground PIN
+        # Ensure the ground PIN is off
         self.off(self.groundPin)
-        #turn on active PIN
+        # Turn on active PIN
         self.on(self.activePin)
         sleep(5)
-        #switch everything off
+        # Turn off every PIN
         self.off(self.groundPin)
         self.off(self.activePin)
 
+    # Inverts the PINs to reverse the current
     def move_backward(self):
         print("Moving motor backward...")
-        #turn off active PIN
+        # Ensure the active PIN is off
         self.off(self.activePin)
-        #turn on ground PIN
+        # Turn on ground PIN
         self.on(self.groundPin)
         sleep(5)
-        #switch everything off
+        #Turn off everything 
         self.off(self.activePin)
         self.off(self.groundPin)
         
