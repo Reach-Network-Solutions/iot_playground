0cde73ee69b80281891a9bddde5590c597f48e0f
polojerry <polojerry4@gmail.com>
2021-04-28 09:10:18 +0300

    added suspersonic sensor module for rapbery pi

---
+++ Pi/ultrasonic-sensor/ultrasonic_sensor.py
@@ -0,0 +1,67 @@
+import RPi.GPIO as GPIO
+import time
+
+class UltrasonicSensor():
+    ''' trig represent the trigger pin,
+        echo represent the echo pin,
+        signal_time represent the time for each signal burst to be sent
+    '''
+    def __init__(self, trig, echo, signal_time):
+        self.trig = trig
+        self.echo = echo
+        self.signal_time = signal_time
+
+        # seting up the pin board mode, and pin
+        # TRIG sends the Output signal
+        # ECHO receives the Input 
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.trig, GPIO.OUT)
+        GPIO.setup(self.echo, GPIO.IN)
+
+    #Sends a Hign signal from the trigger
+    def trigger_high(self):
+        GPIO.output(self.trig, GPIO.HIGH)
+
+    #Sends a Low signal from the trigger
+    def trigger_low(self):
+        GPIO.output(self.trig, GPIO.LOW)
+
+    ''' Sends a high and a low signal from the trigger pin,
+         at a certaion interval set by the signal_time
+    '''
+    def send_signal(self):
+        self.trigger_high()
+        time.sleep(self.signal_time)
+        self.trigger_low()
+
+    #Calculates the distance taken by the signal. Use a constant of 17150 for cm
+    def calculate_distance(self):
+        pulse_duration = self.end_time - self.start_time
+        distance = pulse_duration * 17150
+        return distance
+
+    #Gets the duration of the pulse sent by the trigger and received by the echo
+    def signal_duration(self):
+        while GPIO.input(self.echo) == 0:
+            self.start_time = time.time()
+
+        while GPIO.input(self.echo) == 1:
+            self.end_time = time.time()
+
+
+    #Gets the signal duration and call calculate distacne function retuning the distance
+    def distance(self):
+        self.signal_duration()
+
+        return self.calculate_distance()
+    
+
+#example use case
+sensor = UltrasonicSensor(21, 20, 0.0005)
+
+try :
+    while True:
+        sensor.send_signal()
+        print(sensor.distance())
+except KeyboardInterrupt:
+    GPIO.cleanup
